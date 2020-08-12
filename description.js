138a7962aaafed262bce3f3aa884f1e063bf7d83
ts 21 <shoukitsuda0310@gmail.com>
2020-08-12 19:32:56 +0900

    formated

--- description.js
+++ description.js
@@ -8,9 +8,9 @@ exports.handler = (event, context, callback) => {
     var response = {
         statusCode: 200,
         headers: {
-            "Access-Control-Allow-Origin" : "*"
+            "Access-Control-Allow-Origin": "*"
         },
-        body: JSON.stringify({"message" : ""})
+        body: JSON.stringify({"message": ""})
     };
 
     var body = JSON.parse(event.body);
@@ -18,28 +18,28 @@ exports.handler = (event, context, callback) => {
     //paramに対象のテーブル名とプライマリーキーを記述
     var param = {
         TableName: tableName,
-        Key:{//プライマリキーによって１つ指定
+        Key: {//プライマリキーによって１つ指定
             "product_id": body.id,
         },
     };
 
     //dynamo.get()
-    docClient.get(param, function(err, data){
-        if(err){
+    docClient.get(param, function (err, data) {
+        if (err) {
             response.statusCode = 500;
             response.body = JSON.stringify({"message": "データが見つかりません"});
             callback(null, response);
-            return ;
-        }else{
-            docClient.get(param, function(err, data){
-                if(err){
+            return;
+        } else {
+            docClient.get(param, function (err, data) {
+                if (err) {
                     //TODO: 取得に失敗した場合の処理を記述
                     response.statusCode = 500;
                     //response.body = JSON.stringify({"message": "予期せぬエラーが発生しました"});
                     response.body = JSON.stringify(err);
                     callback(null, response);
                     return;
-                }else{
+                } else {
                     //TODO: 取得に成功した場合の処理を記述
                     response.body = JSON.stringify({"message": "success"});
                     console.log(data);
